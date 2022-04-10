9448dfd521ab2adaba764a011f6bcccef8b8e8a9
coconutstd <wnsspdl@naver.com>
2022-04-11 01:30:48 +0900

    TailwindCSS 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-500 mt-3">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+import path from "path"
+import webpack from "webpack"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import { CleanWebpackPlugin } from "clean-webpack-plugin"
+import "webpack-dev-server"
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}
+
+export default config
