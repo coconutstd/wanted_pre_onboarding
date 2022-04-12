3728e2bb1e750d9b83480aa52c1178e83c348bd4
coconutstd <wnsspdl@naver.com>
2022-04-12 23:47:44 +0900

    인풋 기능 구현

---
+++ src/components/Input.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faCircleCheck,
+  faEyeSlash,
+  faEye,
+} from "@fortawesome/free-solid-svg-icons"
+
+const Input = () => {
+  const [formValue, setFormValue] = useState({ email: "", password: "" })
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  const $password = React.createRef()
+  const $emailCheck = React.createRef()
+  const $errorMessage = React.createRef()
+
+  const onChangeInput = (e) => {
+    const nextFormValue = {
+      ...formValue,
+      [e.target.id]: e.target.value,
+    }
+    setFormValue(nextFormValue)
+    if (e.target.id === "email") emailValidation(e.target.value)
+  }
+
+  const emailValidation = (email) => {
+    if (email.length === 0) {
+      $emailCheck.current.classList.remove("text-green-500")
+    }
+
+    const emailRegex = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    const isOk = emailRegex.test(email)
+    if (isOk) {
+      $emailCheck.current.classList.add("text-green-500")
+      $errorMessage.current.style = "display: none"
+      return true
+    } else {
+      $emailCheck.current.classList.remove("text-green-500")
+      return false
+    }
+  }
+
+  const showErrorMessage = (e) => {
+    if (e.target.value.length === 0) {
+      $errorMessage.current.style = "display: none"
+      return
+    }
+    if (!emailValidation(e.target.value)) {
+      $errorMessage.current.style = "display: block"
+    } else {
+      $errorMessage.current.style = "display: none"
+    }
+  }
+
+  const togglePasswordVisibility = (e) => {
+    $password.current.type =
+      $password.current.type === "password" ? "text" : "password"
+    setIsPasswordVisible($password.current.type === "password" ? false : true)
+  }
+
+  return (
+    <div className="input-component w-1/2 h-36 bg-white-400 border-2 border-black rounded-lg flex-row mt-3 bg-slate-100">
+      <form className="p-3 w-full h-full">
+        <div className="email-box flex flex-col h-1/2 relative">
+          <div
+            ref={$errorMessage}
+            className="absolute left-20 -top-1 text-sm text-red-700"
+            style={{ display: "none" }}
+          >
+            invalid email
+          </div>
+          <label htmlFor="email" className="text-sm text-slate-500">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="email"
+            value={formValue.email}
+            onChange={onChangeInput}
+            onBlur={showErrorMessage}
+            placeholder="E-mail"
+            className={`outline rounded-sm mt-1 p-1 focus:outline-slate-400 focus:outline-2 text-sm px-2`}
+          ></input>
+          <span
+            ref={$emailCheck}
+            className={`absolute right-2`}
+            style={{ top: "1.6rem" }}
+          >
+            <FontAwesomeIcon icon={faCircleCheck} />
+          </span>
+        </div>
+
+        <div className="password-box flex flex-col h-1/2 relative">
+          <label htmlFor="password" className="text-sm text-slate-500">
+            Password
+          </label>
+          <input
+            ref={$password}
+            id="password"
+            type="password"
+            value={formValue.password}
+            onChange={onChangeInput}
+            placeholder="Password"
+            className="outline rounded-sm mt-1 p-1 focus:outline-slate-400 focus:outline-2 text-sm px-2"
+          ></input>
+          <button
+            type="button"
+            className={`absolute`}
+            style={{ top: "1.6rem", right: "0.4rem" }}
+            onClick={togglePasswordVisibility}
+          >
+            <FontAwesomeIcon icon={isPasswordVisible ? faEye : faEyeSlash} />
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Input
