72877dfa958e0335526d32789260614434998910
coconutstd <wnsspdl@naver.com>
2022-04-14 20:15:56 +0900

    탭 기능 리팩터링

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,35 +1,35 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 
 const Tab = () => {
-  const [tabList, setTabList] = useState([
+  const [tabMenus, setTabMenus] = useState([
     { id: 1, text: "감자", isSelected: true },
     { id: 2, text: "고구마", isSelected: false },
     { id: 3, text: "카레라이스", isSelected: false },
   ])
 
-  const $bar = React.createRef()
+  const $bar = useRef(null)
 
   const changeTab = (id) => {
-    const nextTabList = tabList.map((tab) => {
-      if (tab.id === id) {
-        tab.isSelected = true
-      } else {
-        tab.isSelected = false
-      }
+    const nextTabList = tabMenus.map((tab) => {
+      tab.id === id ? (tab.isSelected = true) : (tab.isSelected = false)
       return tab
     })
-    setTabList(nextTabList)
+    setTabMenus(nextTabList)
+    barMove()
+  }
+
+  const barMove = () => {
     $bar.current.style = `transform: translateX(${(getActiveTab() - 1) * 100}%)`
   }
 
   const getActiveTab = () => {
-    return tabList.filter((tab) => tab.isSelected)[0].id
+    return tabMenus.filter((tab) => tab.isSelected)[0].id
   }
 
   return (
-    <div className="tab-menu w-1/2 h-16 bg-white-400 border-2 border-black rounded-lg flex">
+    <div className="tab-menu min-w-[350px] max-w-[500px] w-1/2 h-16 bg-white-400 border-2 border-black rounded-lg flex mt-3">
       <ul className="tab-list relative flex w-full h-full">
-        {tabList.map((tab) => (
+        {tabMenus.map((tab) => (
           <li
             key={tab.id}
             className={`tab-item ${
@@ -42,9 +42,9 @@ const Tab = () => {
         ))}
         <div
           ref={$bar}
-          className={`absolute w-1/3 h-1 bg-blue-400 bottom-px transition duration-700`}
+          className="absolute w-1/3 h-2 bg-blue-400 rounded-lg overflow-hidden bottom-px transition duration-700"
         ></div>
-        <div className="absolute w-full h-1 -z-10 bg-slate-400 bottom-px"></div>
+        <div className="absolute w-full h-2 -z-10 bg-slate-400 bottom-px"></div>
       </ul>
     </div>
   )
