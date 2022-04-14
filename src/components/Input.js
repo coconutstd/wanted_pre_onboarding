64926c480cb031f7509ad8d60a4647f949e3492d
coconutstd <wnsspdl@naver.com>
2022-04-14 20:16:26 +0900

    인풋 기능 리팩터링

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faCircleCheck,
@@ -9,10 +9,10 @@ import {
 const Input = () => {
   const [formValue, setFormValue] = useState({ email: "", password: "" })
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+  const [formValueIsOk, setFormValueIsOk] = useState({ email: false })
 
-  const $password = React.createRef()
-  const $emailCheck = React.createRef()
-  const $errorMessage = React.createRef()
+  const $password = useRef(null)
+  const $errorMessage = useRef(null)
 
   const onChangeInput = (e) => {
     const nextFormValue = {
@@ -20,36 +20,14 @@ const Input = () => {
       [e.target.id]: e.target.value,
     }
     setFormValue(nextFormValue)
-    if (e.target.id === "email") emailValidation(e.target.value)
+    emailValidation(e.target.id, e.target.value)
   }
 
-  const emailValidation = (email) => {
-    if (email.length === 0) {
-      $emailCheck.current.classList.remove("text-green-500")
-    }
-
-    const emailRegex = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+  const emailValidation = (type, email) => {
+    if (type !== "email") return
+    const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
     const isOk = emailRegex.test(email)
-    if (isOk) {
-      $emailCheck.current.classList.add("text-green-500")
-      $errorMessage.current.style = "display: none"
-      return true
-    } else {
-      $emailCheck.current.classList.remove("text-green-500")
-      return false
-    }
-  }
-
-  const showErrorMessage = (e) => {
-    if (e.target.value.length === 0) {
-      $errorMessage.current.style = "display: none"
-      return
-    }
-    if (!emailValidation(e.target.value)) {
-      $errorMessage.current.style = "display: block"
-    } else {
-      $errorMessage.current.style = "display: none"
-    }
+    setFormValueIsOk({ ...formValueIsOk, email: isOk })
   }
 
   const togglePasswordVisibility = (e) => {
@@ -58,14 +36,21 @@ const Input = () => {
     setIsPasswordVisible($password.current.type === "password" ? false : true)
   }
 
+  const showOrHideErrorMessage = () => {
+    if (formValue.email.length === 0 || formValueIsOk.email === true) {
+      $errorMessage.current.classList.add("hidden")
+    } else {
+      $errorMessage.current.classList.remove("hidden")
+    }
+  }
+
   return (
-    <div className="input-component w-1/2 h-36 bg-white-400 border-2 border-black rounded-lg flex-row mt-3 bg-slate-100">
+    <div className="input-component min-w-[350px] max-w-[500px] w-1/2 h-36 bg-white-400 border-2 border-black rounded-lg flex-row mt-3 bg-slate-100">
       <form className="p-3 w-full h-full">
         <div className="email-box flex flex-col h-1/2 relative">
           <div
             ref={$errorMessage}
-            className="absolute left-20 -top-1 text-sm text-red-700"
-            style={{ display: "none" }}
+            className={`hidden absolute left-20 -top-1 text-sm text-red-700`}
           >
             invalid email
           </div>
@@ -77,13 +62,14 @@ const Input = () => {
             type="email"
             value={formValue.email}
             onChange={onChangeInput}
-            onBlur={showErrorMessage}
+            onBlur={showOrHideErrorMessage}
             placeholder="E-mail"
             className={`outline rounded-sm mt-1 p-1 focus:outline-slate-400 focus:outline-2 text-sm px-2`}
           ></input>
           <span
-            ref={$emailCheck}
-            className={`absolute right-2`}
+            className={`absolute right-2 ${
+              formValueIsOk.email ? "text-green-500" : null
+            }`}
             style={{ top: "1.6rem" }}
           >
             <FontAwesomeIcon icon={faCircleCheck} />
