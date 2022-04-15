1a6cabaf426a4fb794f940b0e8ca7469652a2fc8
coconutstd <wnsspdl@naver.com>
2022-04-15 12:14:08 +0900

    슬라이더 기능 픽셀매칭 구현

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -29,16 +29,35 @@ const Slider = () => {
           %
         </div>
       </div>
-      <div className="contorl-pane flex flex-col grow h-1/2 mt-3 p-2">
-        <input
-          type="range"
-          min={1}
-          max={100}
-          value={percentage}
-          step={1}
-          onChange={onChangePercentage}
-          className="slider w-full h-2 rounded-lg cursor-pointer"
-        ></input>
+      <div className="contorl-pane flex flex-col grow h-1/2 p-2">
+        <div className="slider-box relative">
+          <input
+            type="range"
+            min={1}
+            max={100}
+            value={percentage}
+            onChange={onChangePercentage}
+            className="slider w-full h-2 translate-x-[1px] translate-y-[1px] rounded-full z-10 accent-blue-400 cursor-pointer appearance-none bg-transparent"
+          ></input>
+
+          <div
+            className={`absolute slider-bar h-2 rounded-full -translate-y-3 bg-blue-400 -z-10`}
+            style={{ width: `${percentage}%` }}
+          ></div>
+
+          <div className="absolute h-2 w-full bg-gray-200 -translate-y-3 rounded-full -z-20" />
+
+          <div className="flex w-full top-2 absolute justify-between items-center -z-20">
+            {buttons.map((button) => (
+              <div
+                key={button.id}
+                className={`rounded-full w-4 h-4 ${
+                  button.percentage < percentage ? "bg-blue-400" : "bg-gray-200"
+                }`}
+              ></div>
+            ))}
+          </div>
+        </div>
 
         <ul className="percentage-buttons flex justify-between mt-2">
           {buttons.map((button) => (
